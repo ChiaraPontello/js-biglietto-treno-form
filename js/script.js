de1902cb724a40d64f7130667680398c8515df59
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-15 17:14:09 +0200

    html + css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+const kilometri = document.getElementById('kilometri');
+console.log(kilometri);
+
+const selezione = document.getElementById('selezione');
+console.log(selezione);
+
+const btn = document.getElementById('btn');
+const km = 0.21;
+
+const totale = document.querySelector('.totale');
+
+
